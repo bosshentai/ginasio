9b593497e31e2c1593a7bb537395dc56c9d85b37
bosshentai <baptistamhernani@gmail.com>
2021-06-29 13:55:16 -0100

    add class Gym and change some function in register Cliente

--- client/src/sample/controllers/RegisterClienteController.java
+++ client/src/sample/controllers/RegisterClienteController.java
@@ -1,22 +1,26 @@
 package sample.controllers;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import sample.Main;
-import sample.data.Datasource;
-import sample.models.Cliente;
 
+
+import sample.models.Cliente;
 import sample.models.Gym;
-import sample.models.PlanoTreino;
+
 
 import java.io.IOException;
 
-public class RegisterClienteController  {
+public class RegisterClienteController {
 
     Gym gym = new Gym();
+
 
     @FXML
     private TextField firstName;
@@ -31,8 +35,14 @@ public class RegisterClienteController  {
     private TextField phone;
 
 
+    ObservableList<String> options = FXCollections.observableArrayList(
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4"
+    );
     @FXML
-    private ComboBox<PlanoTreino> cbTypeTrainningPlan;
+    ComboBox cbTypeTrainningPlan = new ComboBox(options);
 
     @FXML
     private void swichBack() throws IOException {
@@ -40,46 +50,44 @@ public class RegisterClienteController  {
     }
 
     @FXML
-    private void registerClient() throws IOException{
+    private void registerClient() throws IOException {
         if (firstName.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("O Primeiro nome  encontra vazio");
             alert.showAndWait();
-        }else if (lastName.getText().isEmpty()){
+        } else if (lastName.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("O ultimo nome  encontra vazio");
             alert.showAndWait();
-        }else if (nBI.getText().isEmpty()){
+        } else if (nBI.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("O bi do Personal Trainer encontra vazio");
             alert.showAndWait();
-        }else if (phone.getText().isEmpty()){
+        } else if (phone.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("O numero de telefone encontra vazio");
             alert.showAndWait();
 
-        }else {
+        } else {
+            Cliente cliente = new Cliente(firstName.getText(), lastName.getText(), phone.getText(), Integer.parseInt(nBI.getText()));
 
-
-            if(!gym.addClient(firstName.getText(),lastName.getText(),phone.getText(), nBI.getText())){
+            if (!gym.addClient(cliente)) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setHeaderText("O numero do Bi ja existe no sistema ");
                 alert.showAndWait();
-            }
-            else{
+            } else {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setHeaderText("Registrado com sucesso");
                 alert.showAndWait();
             }
 
 
-
-
-
-
-            }
+        }
 
     }
+
+
+//    cbTypeTrainningPlan.
 
 
 }
--- client/src/sample/models/Gym.java
+++ client/src/sample/models/Gym.java
@@ -2,30 +2,24 @@ package sample.models;
 
 import sample.data.Datasource;
 
-import java.util.ArrayList;
 
 public class Gym {
 
-    //private static Cliente clients = new Cliente();
-    private ArrayList<Cliente> clients = new ArrayList<Cliente>();
 
+    public boolean addClient(Cliente cliente) {
 
-    public boolean addClient(String firstName, String lastName, String phoneNumber,String numberBI){
+        if (verifyClient(cliente.getNumberBI())) {
 
-        if(verifyClient( numberBI)){
-         //   System.out.println("Client exist in the system");
             return false;
-        }else{
-          //  System.out.println("Client dont existe in the system");
-            Datasource.getInstance().registerClient(firstName,lastName,phoneNumber,Integer.parseInt(numberBI));
+        } else {
+
+            Datasource.getInstance().registerClient(cliente.getFirstName(), cliente.getLastName(), cliente.getPhoneNumber(), cliente.getNumberBI());
             return true;
         }
     }
 
 
-    public boolean verifyClient(String numberBI){
-         Integer number =   Integer.parseInt(numberBI);
-        System.out.println(" This test is the number is exist ");
-        return Datasource.getInstance().queryClientNumberBI().contains(number);
+    public boolean verifyClient(Integer numberBI) {
+        return Datasource.getInstance().queryClientNumberBI().contains(numberBI);
     }
 }
